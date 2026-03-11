1d1385210d737cac83820ab729177e33455fbee3
Your Name <hardeymolhar@gmail.com>
2026-03-11 01:47:49 +0100

    Fixing and Revalidating Pipeline Script

--- deployment-pipeline.sh
+++ deployment-pipeline.sh
@@ -13,27 +13,10 @@ terraform -chdir=terraform validate
 echo "Creating execution plan..."
 terraform -chdir=terraform plan \
   -var-file="variables.tfvars" \
-  -parallelism=30 \
+  -parallelism=50 \
   -out=tfplan
 
 echo "Applying Terraform plan..."
 terraform -chdir=terraform apply -auto-approve tfplan
 
-echo "Fetching Terraform outputs..."
-
-VM_IP=$(terraform -chdir=terraform output -raw vm_pip)
-COSMOS_ENDPOINT=$(terraform -chdir=terraform output -raw cosmosdb_uri)
-COSMOS_KEY=$(terraform -chdir=terraform output -raw cosmos_key)
-
-echo "Updating Ansible inventory..."
-cat > ansible/inventory.ini <<EOF
-[cosmos_vm]
-$VM_IP ansible_user=azureuser ansible_ssh_private_key_file=/home/hardeymolhar/.ssh/d3v-u6untu-01_key
-EOF
-
-echo "Running Ansible playbook..."
-
-ansible-playbook ansible/ansible-playbook.yml \
---extra-vars "cosmos_endpoint=$COSMOS_ENDPOINT cosmos_key=$COSMOS_KEY"
-
-echo "Pipeline completed successfully."
+wsl -d ubuntu ./ansible-configuration.sh
