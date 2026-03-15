a03b8c9b118a3216ceaccb4f0f8c8c97b121dfe1
Your Name <hardeymolhar@gmail.com>
2026-03-15 02:00:25 +0100

    Modified log analytics monitor automation script for precision

--- terraform/config-files/log-analytics-monitor.sh
+++ terraform/config-files/log-analytics-monitor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-#First you query the logs with the command "AzureDiagnostics | take 10"
 
 echo "Fetching Log Analytics workspace ID..."
 workspace_id=$(az monitor log-analytics workspace list \
+--resource-group "$(az group list --query "[0].name" -o tsv)" \
 --query "[0].customerId" -o tsv)
+
+echo "Fetched Log Analytics Workspace ID"
 
 echo "Querying Log Analytics for recent logs..."
 az monitor log-analytics query \
@@ -18,4 +20,5 @@ AzureDiagnostics
 | summarize totalRU=sum(todouble(requestCharge_s))
   by bin(TimeGenerated,5m), databaseName_s, collectionName_s
 | order by TimeGenerated desc
-'
+' \
+-o jsonc
