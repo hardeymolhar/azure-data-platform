d0d48b2dce550a92f3fc9caf8187fdd04542f149
Your Name <hardeymolhar@gmail.com>
2026-03-20 05:25:47 +0100

    Updated deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -46,7 +46,7 @@ terraform workspace select $ENV 2>/dev/null || terraform workspace new $ENV
 # ----------------------------------------
 
 
-terraform -chdir=../terraform init -reconfigure \
+terraform -chdir=../terraform init -migrate-state \
   -backend-config="key=${ENV}.terraform.tfstate"
 
 echo -e "\e[33mFormatting Terraform files...\e[0m"
